c9dc69f65d2fdf42f9f4643e60c294e7d6f61041
passwind <mike.zhuyu@gmail.com>
2021-05-24 14:32:07 +0800

    Add cancel auth url funcs

--- auth.go
+++ auth.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type AuthService interface {
 	GetAuthURL() (string,error)
+	GetCancelAuthURL() (string,error)
 }
 
 type AuthServiceOp struct {
@@ -13,6 +14,14 @@ type AuthServiceOp struct {
 func (s *AuthServiceOp)GetAuthURL() (string,error) {
 	rurl := s.client.app.RedirectURL
 	path:="/api/v2/shop/auth_partner"
+	sign,ts,_ := s.client.Util.Sign(path)
+	aurl := fmt.Sprintf("%s%s?partner_id=%d&timestamp=%d&sign=%s&redirect=%s", s.client.app.APIURL,path, s.client.app.PartnerID,ts,sign, rurl)
+	return aurl,nil
+}
+
+func (s *AuthServiceOp)GetCancelAuthURL() (string,error) {
+	rurl := s.client.app.RedirectURL
+	path:="/api/v2/shop/cancel_auth_partner"
 	sign,ts,_ := s.client.Util.Sign(path)
 	aurl := fmt.Sprintf("%s%s?partner_id=%d&timestamp=%d&sign=%s&redirect=%s", s.client.app.APIURL,path, s.client.app.PartnerID,ts,sign, rurl)
 	return aurl,nil
--- auth_test.go
+++ auth_test.go
@@ -9,3 +9,11 @@ func Test_GetAuthURL(t *testing.T) {
 	authURL,_:=client.Auth.GetAuthURL()
 	t.Logf("auth url: %s",authURL)
 } 
+
+func Test_GetCancelAuthURL(t *testing.T) {
+	setup()
+	defer teardown()
+
+	authURL,_:=client.Auth.GetCancelAuthURL()
+	t.Logf("cancel auth url: %s",authURL)
+} 
