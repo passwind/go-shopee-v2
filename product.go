721bbf61c52cb8e8ecce196a10674993cba22ab1
passwind <mike.zhuyu@gmail.com>
2021-05-25 14:45:17 +0800

    Add product.get_brand_list

--- product.go
+++ product.go
@@ -2,6 +2,7 @@ package goshopee
 
 type ProductService interface {
 	GetCategory(int64, string, string) (*GetCategoryResponse,error)
+	GetBrandList(int64, int64, int, int, int, string) (*GetBrandListResponse, error)
 }
 
 type GetCategoryResponse struct {
@@ -30,12 +31,54 @@ type GetCategoryRequest struct {
 	Language string `url:"language"`
 }
 
+type GetBrandListRequest struct {
+	Offset int `url:"offset"`
+	PageSize int `url:"page_size"`
+	CategoryID int64 `url:"category_id"`
+	Status int `url:"status"`
+}
+
+type GetBrandListResponse struct {
+	BaseResponse
+
+	Response GetBrandListResponseData `json:"response"`
+}
+
+type GetBrandListResponseData struct {
+	BrandList []Brand `json:"brand_list"`
+	HasNextPage bool `json:"has_next_page"`
+	NextOffset int `json:"next_offset"`
+	IsMandatory bool `json:"is_mandatory"`
+	InputType string `json:"input_type"`
+}
+
+type Brand struct {
+	BrandID int64 `json:"brand_id"`
+	OriginalBrandName string `json:"original_brand_name"`
+	DisplayBrandName string `json:"display_brand_name"`
+}
+
 func (s *ProductServiceOp)	GetCategory(sid int64, tok, lang string) (*GetCategoryResponse,error){
 	path := "/product/get_category"
 
 	opt:=GetCategoryRequest{lang}
 
 	resp := new(GetCategoryResponse)
+	err := s.client.WithShop(sid,tok).Get(path, resp, opt)
+	return resp, err
+}
+
+func (s *ProductServiceOp)	GetBrandList(sid, cid int64, status, offset, pageSize int, tok string) (*GetBrandListResponse, error){
+	path := "/product/get_brand_list"
+
+	opt:=GetBrandListRequest{
+		CategoryID: cid,
+		Offset: offset,
+		PageSize: pageSize,
+		Status: status,
+	}
+
+	resp := new(GetBrandListResponse)
 	err := s.client.WithShop(sid,tok).Get(path, resp, opt)
 	return resp, err
 }
--- product_test.go
+++ product_test.go
@@ -26,3 +26,23 @@ func Test_GetCategory(t *testing.T) {
 		t.Errorf("CategoryID returned %+v, expected %+v",res.Response.CategoryList[0].CategoryID , expectedID)
 	}
 }
+
+func Test_GetBrandList(t *testing.T) {
+	setup()
+	defer teardown()
+
+	httpmock.RegisterResponder("GET", fmt.Sprintf("%s/api/v2/product/get_brand_list",app.APIURL),
+		httpmock.NewBytesResponder(200, loadFixture("brand_list.json")))
+
+	res,err:=client.Product.GetBrandList(shopID,123,0,10,1,accessToken)
+	if err!=nil {
+		t.Errorf("Product.GetBrandList error: %s",err)
+	}
+
+	t.Logf("Product.GetBrandList: %#v",res)
+
+	var expectedID int64 = 2500139861
+	if res.Response.BrandList[0].BrandID != expectedID {
+		t.Errorf("BrandID returned %+v, expected %+v",res.Response.BrandList[0].BrandID , expectedID)
+	}
+}
