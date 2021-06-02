afa6af9d13d47cb5d8ffa1f0f1c22c6028c16c08
passwind <mike.zhuyu@gmail.com>
2021-06-02 14:35:33 +0800

    Add discount.add_discount_item

--- discount.go
+++ discount.go
@@ -1,11 +1,89 @@
 package goshopee
 
 type DiscountService interface{
+	AddDiscount(uint64, AddDiscountRequest, string) (*AddDiscountResponse, error)
+	AddDiscountItem(uint64, AddDiscountItemRequest, string) (*AddDiscountItemResponse, error)
 	DeleteDiscountItem(uint64, uint64, uint64, uint64, string) (*DeleteDiscountItemResponse, error)
 }
 
 type DiscountServiceOp struct {
 	client *Client
+}
+
+type AddDiscountRequest struct {
+	DiscountName string `json:"discount_name"`
+	StartTime int64 `json:"start_time"`
+	EndTime int64 `json:"end_time"`
+}
+
+type AddDiscountResponse struct {
+	BaseResponse
+
+	Response AddDiscountResponseData `json:"response"`
+}
+
+type AddDiscountResponseData struct {
+	DiscountID uint64 `json:"discount_id"`
+}
+
+func (s *DiscountServiceOp)AddDiscount(sid uint64, data AddDiscountRequest, tok string) (*AddDiscountResponse, error) {
+	path := "/discount/add_discount"
+	resp := new(AddDiscountResponse)
+	req,err:=StructToMap(data)
+	if err!=nil {
+		return nil,err
+	}
+	err = s.client.WithShop(sid,tok).Post(path, req, resp)
+	return resp, err
+}
+
+type AddDiscountItemRequest struct {
+	DiscountID uint64 `json:"discount_id"`
+	ItemList []AddDiscountItemRequestData `json:"item_list"`
+}
+
+type AddDiscountItemRequestData struct {
+	ItemID uint64 `json:"item_id"`
+	ModelList []AddDiscountItemRequestDataModel `json:"model_list"`
+	ItemPromotionPrice float64 `json:"item_promotion_price"`
+	PurchaseLimit int `json:"purchase_limit"`
+	ItemPromotionStock int `json:"item_promotion_stock"`
+}
+
+type AddDiscountItemRequestDataModel struct {
+	ModelID uint64 `json:"model_id"`
+	ModelPromotionPrice float64 `json:"model_promotion_price"`
+	ModelPromotionStock int `json:"model_promotion_stock"`
+}
+
+type AddDiscountItemResponse struct {
+	BaseResponse
+
+	Response AddDiscountItemResponseData `json:"response"`
+}
+
+type AddDiscountItemResponseData struct {
+	DiscountID uint64 `json:"discount_id"`
+	Count int `json:"count"`
+	ErrorList []AddDiscountItemResponseDataError `json:"error_list"`
+}
+
+type AddDiscountItemResponseDataError struct {
+	ItemID uint64 `json:"item_id"`
+	ModelID uint64 `json:"model_id"`
+	FailMessage string `json:"fail_message"`
+	FailError string `json:"fail_error"`
+}
+
+func (s *DiscountServiceOp)AddDiscountItem(sid uint64, data AddDiscountItemRequest, tok string) (*AddDiscountItemResponse, error) {
+	path := "/discount/add_discount_item"
+	resp := new(AddDiscountItemResponse)
+	req,err:=StructToMap(data)
+	if err!=nil {
+		return nil,err
+	}
+	err = s.client.WithShop(sid,tok).Post(path, req, resp)
+	return resp, err
 }
 
 type DeleteDiscountItemResponse struct {
--- discount_test.go
+++ discount_test.go
@@ -7,6 +7,52 @@ import (
 	"github.com/jarcoal/httpmock"
 )
 
+func Test_AddDiscount(t *testing.T) {
+	setup()
+	defer teardown()
+
+	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/add_discount",app.APIURL),
+		httpmock.NewBytesResponder(200, loadFixture("add_discount_resp.json")))
+
+	var req AddDiscountRequest
+	loadMockData("add_discount_req.json",&req)
+
+	res,err:=client.Discount.AddDiscount(shopID,req,accessToken)
+	if err!=nil {
+		t.Errorf("Discount.AddDiscount error: %s",err)
+	}
+
+	t.Logf("Discount.AddDiscount: %#v",res)
+
+	var expectedID uint64 = 122131231
+	if res.Response.DiscountID != expectedID {
+		t.Errorf("DiscountID returned %+v, expected %+v", res.Response.DiscountID , expectedID)
+	}
+}
+
+func Test_AddDiscountItem(t *testing.T) {
+	setup()
+	defer teardown()
+
+	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/add_discount_item",app.APIURL),
+		httpmock.NewBytesResponder(200, loadFixture("add_discount_item_resp.json")))
+
+	var req AddDiscountItemRequest
+	loadMockData("add_discount_item_req.json",&req)
+
+	res,err:=client.Discount.AddDiscountItem(shopID,req,accessToken)
+	if err!=nil {
+		t.Errorf("Discount.AddDiscountItem error: %s",err)
+	}
+
+	t.Logf("Discount.AddDiscountItem: %#v",res)
+
+	var expected string = "discount.error_time"
+	if res.Response.ErrorList[0].FailError != expected {
+		t.Errorf("FailError returned %+v, expected %+v", res.Response.ErrorList[0].FailError , expected)
+	}
+}
+
 func Test_DeleteDiscountItem(t *testing.T) {
 	setup()
 	defer teardown()
