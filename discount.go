05e3f6a9567fb143c1c3935f18bd42731982cb0a
mike.zhuyu <passtravellor@qq.com>
2022-01-25 16:17:15 +0800

    Add update_discount_item

--- discount.go
+++ discount.go
@@ -1,9 +1,10 @@
 package goshopee
 
-type DiscountService interface{
+type DiscountService interface {
 	AddDiscount(uint64, AddDiscountRequest, string) (*AddDiscountResponse, error)
 	AddDiscountItem(uint64, AddDiscountItemRequest, string) (*AddDiscountItemResponse, error)
 	DeleteDiscountItem(uint64, uint64, uint64, uint64, string) (*DeleteDiscountItemResponse, error)
+	UpdateDiscountItem(uint64, UpdateDiscountItemRequest, string) (*UpdateDiscountItemResponse, error)
 }
 
 type DiscountServiceOp struct {
@@ -12,8 +13,8 @@ type DiscountServiceOp struct {
 
 type AddDiscountRequest struct {
 	DiscountName string `json:"discount_name"`
-	StartTime int64 `json:"start_time"`
-	EndTime int64 `json:"end_time"`
+	StartTime    int64  `json:"start_time"`
+	EndTime      int64  `json:"end_time"`
 }
 
 type AddDiscountResponse struct {
@@ -26,34 +27,34 @@ type AddDiscountResponseData struct {
 	DiscountID uint64 `json:"discount_id"`
 }
 
-func (s *DiscountServiceOp)AddDiscount(sid uint64, data AddDiscountRequest, tok string) (*AddDiscountResponse, error) {
+func (s *DiscountServiceOp) AddDiscount(sid uint64, data AddDiscountRequest, tok string) (*AddDiscountResponse, error) {
 	path := "/discount/add_discount"
 	resp := new(AddDiscountResponse)
-	req,err:=StructToMap(data)
-	if err!=nil {
-		return nil,err
+	req, err := StructToMap(data)
+	if err != nil {
+		return nil, err
 	}
-	err = s.client.WithShop(sid,tok).Post(path, req, resp)
+	err = s.client.WithShop(sid, tok).Post(path, req, resp)
 	return resp, err
 }
 
 type AddDiscountItemRequest struct {
-	DiscountID uint64 `json:"discount_id"`
-	ItemList []AddDiscountItemRequestData `json:"item_list"`
+	DiscountID uint64                       `json:"discount_id"`
+	ItemList   []AddDiscountItemRequestData `json:"item_list"`
 }
 
 type AddDiscountItemRequestData struct {
-	ItemID uint64 `json:"item_id"`
-	ModelList []AddDiscountItemRequestDataModel `json:"model_list"`
-	ItemPromotionPrice *float64 `json:"item_promotion_price,omitempty"`
-	PurchaseLimit int `json:"purchase_limit"`
-	ItemPromotionStock *int `json:"item_promotion_stock,omitempty"`
+	ItemID             uint64                            `json:"item_id"`
+	ModelList          []AddDiscountItemRequestDataModel `json:"model_list"`
+	ItemPromotionPrice *float64                          `json:"item_promotion_price,omitempty"`
+	PurchaseLimit      int                               `json:"purchase_limit"`
+	ItemPromotionStock *int                              `json:"item_promotion_stock,omitempty"`
 }
 
 type AddDiscountItemRequestDataModel struct {
-	ModelID uint64 `json:"model_id"`
+	ModelID             uint64  `json:"model_id"`
 	ModelPromotionPrice float64 `json:"model_promotion_price"`
-	ModelPromotionStock int `json:"model_promotion_stock"`
+	ModelPromotionStock int     `json:"model_promotion_stock"`
 }
 
 type AddDiscountItemResponse struct {
@@ -63,26 +64,26 @@ type AddDiscountItemResponse struct {
 }
 
 type AddDiscountItemResponseData struct {
-	DiscountID uint64 `json:"discount_id"`
-	Count int `json:"count"`
-	ErrorList []AddDiscountItemResponseDataError `json:"error_list"`
+	DiscountID uint64                             `json:"discount_id"`
+	Count      int                                `json:"count"`
+	ErrorList  []AddDiscountItemResponseDataError `json:"error_list"`
 }
 
 type AddDiscountItemResponseDataError struct {
-	ItemID uint64 `json:"item_id"`
-	ModelID uint64 `json:"model_id"`
+	ItemID      uint64 `json:"item_id"`
+	ModelID     uint64 `json:"model_id"`
 	FailMessage string `json:"fail_message"`
-	FailError string `json:"fail_error"`
+	FailError   string `json:"fail_error"`
 }
 
-func (s *DiscountServiceOp)AddDiscountItem(sid uint64, data AddDiscountItemRequest, tok string) (*AddDiscountItemResponse, error) {
+func (s *DiscountServiceOp) AddDiscountItem(sid uint64, data AddDiscountItemRequest, tok string) (*AddDiscountItemResponse, error) {
 	path := "/discount/add_discount_item"
 	resp := new(AddDiscountItemResponse)
-	req,err:=StructToMap(data)
-	if err!=nil {
-		return nil,err
+	req, err := StructToMap(data)
+	if err != nil {
+		return nil, err
 	}
-	err = s.client.WithShop(sid,tok).Post(path, req, resp)
+	err = s.client.WithShop(sid, tok).Post(path, req, resp)
 	return resp, err
 }
 
@@ -93,25 +94,76 @@ type DeleteDiscountItemResponse struct {
 }
 
 type DeleteDiscountItemResponseData struct {
-	DiscountID uint64 `json:"discount_id"`
-	ErrorList []DeleteDiscountItemResponseDataError `json:"error_list"`
+	DiscountID uint64                                `json:"discount_id"`
+	ErrorList  []DeleteDiscountItemResponseDataError `json:"error_list"`
 }
 
 type DeleteDiscountItemResponseDataError struct {
-	ItemID uint64 `json:"item_id"`
-	ModelID uint64 `json:"model_id"`
+	ItemID      uint64 `json:"item_id"`
+	ModelID     uint64 `json:"model_id"`
 	FailMessage string `json:"fail_message"`
-	FailError string `json:"fail_error"`
+	FailError   string `json:"fail_error"`
 }
 
-func (s *DiscountServiceOp)DeleteDiscountItem(sid, discountID, itemID, modelID uint64, tok string) (*DeleteDiscountItemResponse, error) {
+func (s *DiscountServiceOp) DeleteDiscountItem(sid, discountID, itemID, modelID uint64, tok string) (*DeleteDiscountItemResponse, error) {
 	path := "/discount/delete_discount_item"
 	wrappedData := map[string]interface{}{
 		"discount_id": discountID,
-		"item_id": itemID,
-		"model_id":  modelID,
+		"item_id":     itemID,
+		"model_id":    modelID,
 	}
 	resp := new(DeleteDiscountItemResponse)
-	err := s.client.WithShop(sid,tok).Post(path, wrappedData, resp)
+	err := s.client.WithShop(sid, tok).Post(path, wrappedData, resp)
+	return resp, err
+}
+
+type UpdateDiscountItemRequest struct {
+	DiscountID uint64                          `json:"discount_id"`
+	ItemList   []UpdateDiscountItemRequestItem `json:"item_list"`
+}
+
+// type UpdateDiscountItemRequestItem struct {
+// 	ItemID uint64 `json:"item_id"`
+// 	// ItemPromotionPrice float64                          `json:"item_promotion_price"`
+// 	// PurchaseLimit      int                              `json:"purchase_limit"`
+// 	ModelList []UpdateDiscountItemRequestModel `json:"model_list"`
+// }
+
+// cause item_promotion_price & purchase_limit are optional
+// if not needed, must not set them in parameters
+type UpdateDiscountItemRequestItem map[string]interface{}
+
+type UpdateDiscountItemRequestModel struct {
+	ModelID             uint64  `json:"model_id"`
+	ModelPromotionPrice float64 `json:"model_promotion_price"`
+}
+
+type UpdateDiscountItemResponse struct {
+	BaseResponse
+
+	Response UpdateDiscountItemResponseData `json:"response"`
+}
+
+type UpdateDiscountItemResponseData struct {
+	DiscountID uint64                                `json:"discount_id"`
+	Count      int                                   `json:"count"`
+	ErrorList  []UpdateDiscountItemResponseDataError `json:"error_list"`
+}
+
+type UpdateDiscountItemResponseDataError struct {
+	ItemID      uint64 `json:"item_id"`
+	ModelID     uint64 `json:"model_id"`
+	FailMessage string `json:"fail_message"`
+	FailError   string `json:"fail_error"`
+}
+
+func (s *DiscountServiceOp) UpdateDiscountItem(sid uint64, data UpdateDiscountItemRequest, tok string) (*UpdateDiscountItemResponse, error) {
+	path := "/discount/update_discount_item"
+	resp := new(UpdateDiscountItemResponse)
+	req, err := StructToMap(data)
+	if err != nil {
+		return nil, err
+	}
+	err = s.client.WithShop(sid, tok).Post(path, req, resp)
 	return resp, err
 }
--- discount_test.go
+++ discount_test.go
@@ -11,22 +11,22 @@ func Test_AddDiscount(t *testing.T) {
 	setup()
 	defer teardown()
 
-	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/add_discount",app.APIURL),
+	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/add_discount", app.APIURL),
 		httpmock.NewBytesResponder(200, loadFixture("add_discount_resp.json")))
 
 	var req AddDiscountRequest
-	loadMockData("add_discount_req.json",&req)
+	loadMockData("add_discount_req.json", &req)
 
-	res,err:=client.Discount.AddDiscount(shopID,req,accessToken)
-	if err!=nil {
-		t.Errorf("Discount.AddDiscount error: %s",err)
+	res, err := client.Discount.AddDiscount(shopID, req, accessToken)
+	if err != nil {
+		t.Errorf("Discount.AddDiscount error: %s", err)
 	}
 
-	t.Logf("Discount.AddDiscount: %#v",res)
+	t.Logf("Discount.AddDiscount: %#v", res)
 
 	var expectedID uint64 = 122131231
 	if res.Response.DiscountID != expectedID {
-		t.Errorf("DiscountID returned %+v, expected %+v", res.Response.DiscountID , expectedID)
+		t.Errorf("DiscountID returned %+v, expected %+v", res.Response.DiscountID, expectedID)
 	}
 }
 
@@ -34,22 +34,22 @@ func Test_AddDiscountItem(t *testing.T) {
 	setup()
 	defer teardown()
 
-	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/add_discount_item",app.APIURL),
+	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/add_discount_item", app.APIURL),
 		httpmock.NewBytesResponder(200, loadFixture("add_discount_item_resp.json")))
 
 	var req AddDiscountItemRequest
-	loadMockData("add_discount_item_req.json",&req)
+	loadMockData("add_discount_item_req.json", &req)
 
-	res,err:=client.Discount.AddDiscountItem(shopID,req,accessToken)
-	if err!=nil {
-		t.Errorf("Discount.AddDiscountItem error: %s",err)
+	res, err := client.Discount.AddDiscountItem(shopID, req, accessToken)
+	if err != nil {
+		t.Errorf("Discount.AddDiscountItem error: %s", err)
 	}
 
-	t.Logf("Discount.AddDiscountItem: %#v",res)
+	t.Logf("Discount.AddDiscountItem: %#v", res)
 
 	var expected string = "discount.error_time"
 	if res.Response.ErrorList[0].FailError != expected {
-		t.Errorf("FailError returned %+v, expected %+v", res.Response.ErrorList[0].FailError , expected)
+		t.Errorf("FailError returned %+v, expected %+v", res.Response.ErrorList[0].FailError, expected)
 	}
 }
 
@@ -57,18 +57,41 @@ func Test_DeleteDiscountItem(t *testing.T) {
 	setup()
 	defer teardown()
 
-	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/delete_discount_item",app.APIURL),
+	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/delete_discount_item", app.APIURL),
 		httpmock.NewBytesResponder(200, loadFixture("delete_discount_item_resp.json")))
 
-	res,err:=client.Discount.DeleteDiscountItem(shopID,1000029882,1776783,1467683,accessToken)
-	if err!=nil {
-		t.Errorf("Discount.DeleteDiscountItem error: %s",err)
+	res, err := client.Discount.DeleteDiscountItem(shopID, 1000029882, 1776783, 1467683, accessToken)
+	if err != nil {
+		t.Errorf("Discount.DeleteDiscountItem error: %s", err)
 	}
 
-	t.Logf("Discount.DeleteDiscountItem: %#v",res)
+	t.Logf("Discount.DeleteDiscountItem: %#v", res)
 
 	var expected string = "time error"
 	if res.Response.ErrorList[0].FailMessage != expected {
-		t.Errorf("FailMessage returned %+v, expected %+v",res.Response.ErrorList[0].FailMessage , expected)
+		t.Errorf("FailMessage returned %+v, expected %+v", res.Response.ErrorList[0].FailMessage, expected)
+	}
+}
+
+func Test_UpdateDiscountItem(t *testing.T) {
+	setup()
+	defer teardown()
+
+	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/api/v2/discount/update_discount_item", app.APIURL),
+		httpmock.NewBytesResponder(200, loadFixture("update_discount_item_resp.json")))
+
+	var req UpdateDiscountItemRequest
+	loadMockData("update_discount_item_req.json", &req)
+
+	res, err := client.Discount.UpdateDiscountItem(shopID, req, accessToken)
+	if err != nil {
+		t.Errorf("Discount.UpdateDiscountItem error: %s", err)
+	}
+
+	t.Logf("Discount.UpdateDiscountItem: %#v", res)
+
+	var expected string = "time error"
+	if res.Response.ErrorList[0].FailMessage != expected {
+		t.Errorf("FailMessage returned %+v, expected %+v", res.Response.ErrorList[0].FailMessage, expected)
 	}
 }
